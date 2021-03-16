5b0ca572ae140c1137ada1c084f05d16e2ad72a9
niraj2998 <66107434+niraj2998@users.noreply.github.com>
2021-03-16 22:44:04 +0530

    added react-loading

--- src/Loading.js
+++ src/Loading.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import ReactLoading from 'react-loading';
 
 const Loading = () => {
   return (
     <div className="loading">
-      <h1>loading...</h1>
+      <ReactLoading className="load" type={'bars'} color={'hsl(205, 63%, 48%)'} height={'60%'} width={'60%'} /><h1>Loading...</h1>
     </div>
   );
 };
